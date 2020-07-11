eff38e8f8cb1d10c0aa9a6bbd4308104e4133d22
Akihide Tsue <tsue_akihide@emplayee.co.jp>
2020-07-11 06:25:05 +0000

    button

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,3 +1,5 @@
 export { default as Logo } from '../../components/Logo.vue'
+export { default as MyButton } from '../../components/MyButton.vue'
 
 export const LazyLogo = import('../../components/Logo.vue' /* webpackChunkName: "components/Logo'}" */).then(c => c.default || c)
+export const LazyMyButton = import('../../components/MyButton.vue' /* webpackChunkName: "components/MyButton'}" */).then(c => c.default || c)
--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,24 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        nuxtはじめました！！
-
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <MyButton />
+      <MyButton back_color="#666666" />
+      <MyButton back_color="#330000" fore_color="#ffeeee" />
+      <MyButton left_arrow />
+      <MyButton right_arrow />
+      <MyButton left_arrow right_arrow />
+      <MyButton>実行</MyButton>
     </div>
   </div>
 </template>
 
 <script>
-
-
-
-
-export default {}
+import MyButton from "~/components/MyButton.vue";
+export default {
+  components: {
+    MyButton
+  }
+};
 </script>
 
 <style>
@@ -44,35 +29,5 @@ export default {}
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
